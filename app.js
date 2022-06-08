76c733724655d111b0ec9e061915b9b91342ccaf
Daniel <bakunzid@gmail.com>
2022-06-07 21:48:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+/* let batton1 = document.querySelector('#baton1');
+batton1.addEventListener('click', function () {
+
+    let clickedbutton = button.value;
+
+    let clickedbuttonEL = document.querySelector('#result');
+    clickedbuttonEL.innerHTML = clickedbutton;
+    
+}) */
+
+
+/* let btn = document.querySelectorAll('#baton1')
+
+val = document.querySelector('#result');
+btn.forEach((number) =>{
+
+    number.addEventListener('click',()=>{
+        val.innerHTML = number.result;
+    })
+}) */
